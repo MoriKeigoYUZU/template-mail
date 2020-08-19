afcb39395eb642c8e599f12bcd00cab07cbd6c0c
AkariUC <akari@pluslab.org>
2020-08-19 23:07:22 +0900

    ADD result pages

--- template-mail/src/router/index.js
+++ template-mail/src/router/index.js
@@ -4,6 +4,10 @@ import VueRouter from 'vue-router'
 import Top from '../pages/Top'
 import MyInformation from '../pages/MyInformation'
 import DestinationInformation from '../pages/DestinationInformation'
+import Result from '../pages/Result'
+import SuperiorResult from '../pages/SuperiorResult'
+import FriendResult from '../pages/FriendResult'
+import RelativeResult from '../pages/RelativeResult'
 
 import Main from '../pages/Main'
 import Props from '../pages/Props'
@@ -11,32 +15,51 @@ import Props from '../pages/Props'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Top',
-    component: Top
-  },
-  {
-    path: '/MyInformation',
-    name: 'MyInformation',
-    component: MyInformation
-  },
-  {
-    path: '/DestinationInformation',
-    name: 'DestinationInformation',
-    component: DestinationInformation
-  },
-  {
-    path: '/Main',
-    name: 'Main',
-    component: Main
-  },
-  {
-    path: '/Props',
-    name: 'Props',
-    component: Props
-  },
+const routes = [{
+        path: '/',
+        name: 'Top',
+        component: Top
+    },
+    {
+        path: '/MyInformation',
+        name: 'MyInformation',
+        component: MyInformation
+    },
+    {
+        path: '/DestinationInformation',
+        name: 'DestinationInformation',
+        component: DestinationInformation
+    },
+    {
+        path: '/Result',
+        name: 'Result',
+        component: Result
+    },
+    {
+        path: '/SuperiorResult',
+        name: 'SuperiorResult',
+        component: SuperiorResult
+    },
+    {
+        path: '/FriendResult',
+        name: 'FriendResult',
+        component: FriendResult
+    },
+    {
+        path: '/RelativeResult',
+        name: 'RelativeResult',
+        component: RelativeResult
+    },
+    {
+        path: '/Main',
+        name: 'Main',
+        component: Main
+    },
+    {
+        path: '/Props',
+        name: 'Props',
+        component: Props
+    },
 ]
 
 // const router = new VueRouter({
@@ -49,16 +72,16 @@ const routes = [
 
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return {x: 0, y: 0}
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { x: 0, y: 0 }
+        }
     }
-  }
 })
 
 export default router
