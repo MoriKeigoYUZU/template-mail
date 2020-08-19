c4165e27a19853c8b836f6af2a77d85908b1e1ee
Mori Keigo <yuzumori.keigo@gmail.com>
2020-08-20 00:26:18 +0900

    ADD myデータをストアで保持

--- template-mail/src/store/index.js
+++ template-mail/src/store/index.js
@@ -3,13 +3,71 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// export default new Vuex.Store({
 export default new Vuex.Store({
+// const store = new Vuex.Store({
   state: {
+    counter: 1,
+
+    flag: 1,
+
+
+    My: {
+      name: "確認用",
+      department: "相手先",
+      tel: "0800606006060",
+      email: "kkk@gmail.com",
+    },
+
+
+    Destination: {
+      name: "mormormormo",
+      department: "ds",
+      select: "gre",
+    },
+
+
   },
+
+  getters: {
+
+    counter(state) {
+      return state.counter
+    },
+
+    flag(state) {
+      return state.flag
+    },
+
+    My(state) {
+      return state.My
+    },
+
+    Destination(state) {
+      return state.Destination
+    },
+
+
+
+  },
+
   mutations: {
+
+    countUp(state) {
+      state.counter++;
+    },
+
+    nameUpdata(state, items) {
+      state.My = items;
+    },
+
+    flagUpdata(state, flag) {
+      state.flag = flag;
+    }
+
   },
-  actions: {
-  },
-  modules: {
-  }
+
+  actions: {},
+
+  modules: {},
 })
